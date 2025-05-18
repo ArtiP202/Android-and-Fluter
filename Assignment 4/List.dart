6104b4ac4e9cbdd518a29b2ddf5a460e5ac691ad
ArtiP202 <artipawar2125@gmail.com>
2025-05-18 20:39:29 +0530

    Delete Assignment 4/List.dart

--- Assignment 4/List.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-void main(){
-  List listofdata=['Shubhra','Arti','Nilesh'];
- for (int i = 0; i < listofdata.length; i++) print(listofdata[i]);
-
-  print(listofdata);
-
-  print(listofdata[3]);
-}
