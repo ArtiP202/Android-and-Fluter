05e9f512111d5333be950d3cf7cadf917fbb78fb
ArtiP202 <artipawar2125@gmail.com>
2025-05-19 17:59:31 +0530

    Add files via upload

---
+++ Assignment 3/Map.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main(){
+
+Map<String,String>Stud1={
+
+    "1st_Stud_Name":"Shiv",
+    "1st_Stud_Cource":"BCS"
+    
+};
+print('${Stud1['1st_Stud_Name']}');
+print('${Stud1['1st_Stud_Cource']}');
+
+}
